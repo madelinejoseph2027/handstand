da82783921e81b5cfaa83affbf5b64b28a64d426
madelinejoseph2027 <122245493+madelinejoseph2027@users.noreply.github.com>
2023-02-06 01:45:45 -0600

    Delete Desktop/handstand directory

--- Desktop/handstand/simulate.py
+++
@@ -1,9 +0,0 @@
-import sys
-from simulation import SIMULATION
-
-
-directOrGUI = sys.argv[1]
-solutionID = sys.argv[2]
-simulation = SIMULATION(directOrGUI, solutionID)
-simulation.Run()
-simulation.Get_Fitness()
